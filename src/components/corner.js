fa9a2b737402edf36588f2bf9d8bfcf051a86f01
Cameron Day <32853335+ConanDW@users.noreply.github.com>
2021-04-22 11:45:40 -0400

    content update

--- src/components/corner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import GitHubCorners from '@uiw/react-github-corners';
-const corner = () => (
-    <GitHubCorners
-      position="right"
-      href="https://github.com/ConanDW/conandw.github.io"
-    />
-)
-export default corner
