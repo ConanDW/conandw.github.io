cf7a99ed1746700e8f81f97c7ec53b04c15bb7a8
Cameron Day <32853335+ConanDW@users.noreply.github.com>
2025-02-11 18:39:04 -0500

    repush

---
+++ src/components/corner.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import GithubCorner from 'react-github-corner'
+export default class corner extends Component {
+  render() {
+    return (
+      <div>
+        <GithubCorner href="https://github.com/ConanDW" />
+      </div>
+    )
+  }
+}
